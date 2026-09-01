e3f5c25c4752e202f471be85d5aebd430086a0c9
Devam Parekh <devamparekh04@gmail.com>
2026-09-01 18:17:26 +0530

    Time: 0 ms (100%), Space: 43 MB (19.22%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,18 @@
+class Solution {
+    // public int goo(int i,int[] nums){
+    //     int n=nums.length;
+    //     if(i+2>=n) return i;
+    //     return goo(i,nums)+goo(i+2,nums);
+    // }
+    public int rob(int[] arr) {
+        int n=arr.length;
+      if(n==1) return arr[0];
+      int[] dp=new int[n];
+      dp[0]=arr[0];
+      dp[1]=Math.max(arr[0],arr[1]);
+      for(int i=2;i<n;i++){
+        dp[i]=Math.max(dp[i-1],dp[i-2]+arr[i]);
+      }
+return dp[n-1];
+    }
+}
